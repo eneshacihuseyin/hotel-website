b33f7f4db44d9b45d55189858a44f5cd5d304efe
eneshacihuseyin <e.hacihuseyin03@gmail.com>
2025-03-08 20:43:55 +0300

    Fixed Bugs About Localization

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -6,63 +6,63 @@ import {IoEarthOutline} from "react-icons/io5";
 import {useTranslation} from "react-i18next";
 import {TbAirConditioning, TbParking, TbSwimming, TbToolsKitchen2, TbWifi} from "react-icons/tb";
 import {FaBath, FaBed, FaChild} from "react-icons/fa";
+import {useLanguage} from "@/components/ui/language-provider.jsx";
 
-function Header({sendDataToParent}) {
+function Header() {
 
-    const [selectedLn,setSelectedLn] = useState(null)
+    const {language, setLanguage} = useLanguage()
     const { t, i18n } = useTranslation();
+    const [opacity, setOpacity] = useState("opacity-0")
 
     useEffect(() => {
-        const selectedLanguage = localStorage.getItem("language")
-        setSelectedLn(selectedLanguage)
-        i18n.changeLanguage(selectedLanguage)
-    },[])
+        setTimeout(()=> setOpacity(""),500)
+    })
 
     const features = [
         {
-            nameEn: "Free Breakfast",
-            nameTr:"Ücretsiz Kahvaltı",
-            icon: <TbToolsKitchen2 size={52} className="text-red-500"/>
+            nameEn: "Breakfast",
+            nameTr:"Kahvaltı",
+            icon: <TbToolsKitchen2 size={24}/>
         },
         {
             nameEn: "Wifi",
             nameTr:"Wifi",
-            icon: <TbWifi size={52} className="text-green-500"/>
+            icon: <TbWifi size={24}/>
         },
         {
             nameEn:"AC",
             nameTr:"Klima",
-            icon: <TbAirConditioning size={52} className="text-blue-500"/>
+            icon: <TbAirConditioning size={24}/>
         },
         {
             nameEn: "Turkish Bath",
             nameTr:"Hamam",
-            icon: <FaBath size={52} className="text-orange-400"/>
+            icon: <FaBath size={24}/>
         },
         {
             nameEn:"Playground",
             nameTr:"Oyun Parkı",
-            icon:<FaChild size={52} className="text-teal-500"/>
+            icon:<FaChild size={24}/>
         },
         {
             nameEn: "Free Parking Lot",
             nameTr:"Ücretsiz Otopark",
-            icon: <TbParking size={52} className="text-purple-500" />
+            icon: <TbParking size={24}/>
         },
         {
             nameEn: "Swimming",
             nameTr:"Yüzme Havuzu",
-            icon: <TbSwimming size={52} className="text-yellow-500"/>
+            icon: <TbSwimming size={24}/>
         },
         {
             nameEn:"Cozy Rooms",
             nameTr:"Rahat Odalar",
-            icon: <FaBed size={52} className="text-pink-500"/>
+            icon: <FaBed size={24}/>
         }
     ]
 
     return (
-        <div className="header w-screen relative flex justify-center mt-[10vh] select-none mb-[10vh]">
+        <div className="header w-screen relative flex justify-center mt-[10vh] select-none mb-[15vh]">
             <div className={`links z-[100] transition-all bg-background duration-200 fixed top-0 left-[5vw] w-[90vw] h-[10vh] flex justify-between items-center`}>
                 <div className="left">
                     <div className="logo h-full text-[2rem] items-center flex ml-[1vw]">
@@ -86,7 +86,7 @@ function Header({sendDataToParent}) {
                         <DropdownMenu className="language">
                             <DropdownMenuTrigger className="flex items-center gap-2  mr-[1vw] cursor-pointer">
                                 <IoEarthOutline size={24} className=""/>
-                                <div className="text-lg">{selectedLn === "en" ? "EN" : "TR"}</div>
+                                <div className="text-lg">{language === "en" ? "EN" : "TR"}</div>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent>
                                 <DropdownMenuItem className="p-0">
@@ -94,10 +94,9 @@ function Header({sendDataToParent}) {
                                         variant="ghost"
                                         className="w-full flex justify-start text-[16px] cursor-pointer"
                                         onClick={() => {
-                                            localStorage.setItem("language", "en")
-                                            setSelectedLn("en")
+                                            setLanguage("en")
+                                            localStorage.setItem("language","en")
                                             i18n.changeLanguage("en")
-                                            sendDataToParent("en")
                                         }}>
                                         {t("home.header.links.changeLn.en")}
                                     </Button>
@@ -107,10 +106,9 @@ function Header({sendDataToParent}) {
                                         variant="ghost"
                                         className="w-full flex justify-start text-[16px] cursor-pointer"
                                         onClick={() => {
-                                            localStorage.setItem("language", "tr")
-                                            setSelectedLn("tr")
+                                            setLanguage("tr")
+                                            localStorage.setItem("language","tr")
                                             i18n.changeLanguage("tr")
-                                            sendDataToParent("tr")
                                         }}>
                                         {t("home.header.links.changeLn.tr")}
                                     </Button>
@@ -125,26 +123,26 @@ function Header({sendDataToParent}) {
             <div className="hero bg-[url(images/9.jpg)] bg-no-repeat bg-cover bg-center w-[90vw] h-[75vh] relative rounded-2xl">
                 <div className="filter absolute top-0 left-0 w-full h-full rounded-2xl"
                      style={{
-                         background: "linear-gradient(90deg, rgba(99,108,175,1) 30%, rgba(99,108,175,0.67) 45%, rgba(99,108,175,0.33) 60%, rgba(99,108,175,0) 75%)"
+                         background: "linear-gradient(90deg, rgba(99,108,175,1) 30%, rgba(99,108,175,0.67) 40%, rgba(99,108,175,0.33) 60%, rgba(99,108,175,0)70%)"
                      }}></div>
-                <div className="slogan w-[40vw] h-full z-10 relative mt-[20vh] ml-[5vw] text-background justify-between">
+                <div className={`slogan w-[42vw] duration-2000 z-10 relative mt-[20vh] ml-[5vw] text-background opa justify-between ${opacity}` }>
                     <div className="text-[3.2rem] font-[500]  flex-[2] ml-[2vw] mt-[1vh] mb-[4vh] leading-tight">
                         {t("home.header.slogan")}
                     </div>
                 </div>
-                <div className="features bg-white w-[80vw] h-[25vh] absolute flex flex-col items-center bottom-[-8vh] pb-[3vh] pt-[1vh] px-[3vh] rounded-2xl shadow-xl border-2 border-[rgba(99,108,175,.5)] left-[5vw]">
-                    <div className="title text-[1.5rem] font-[500] mb-[1vh]">
+                <div className="features bg-white w-[80vw] h-[20vh] absolute flex flex-row gap-[2vw] items-center bottom-[-6vh] px-[2vw] py-[1vh] rounded-2xl shadow-xl border border-[rgba(99,108,175,.5)] left-[5vw]">
+                    <div className="title h-full text-center mt-[8vh] flex-1 text-[1.5rem] font-[500] mb-[1vh]">
                         {t("home.header.features.title")}
                     </div>
-                    <div className="flex w-full h-full justify-between items-center">
+                    <div className="grid grid-cols-4 flex-[3] w-full h-full justify-between items-center">
                         {
                             features.map((feature,index) => (
-                                <div className="feature w-[10vw] flex items-center flex-col gap-2" key={index}>
-                                    <div className="icon p-3 rounded-2xl shadow-lg border-2 border-[rgba(120,129,196,.3)]">
+                                <div className="feature w-[10vw] flex items-center flex-row gap-2" key={index}>
+                                    <div className="icon p-3 rounded-2xl shadow-lg border text-cyan-700 border-[rgba(120,129,196,.3)]">
                                         {feature.icon}
                                     </div>
                                     <div className="text font-[500]">
-                                        {selectedLn === "en" ? feature.nameEn : feature.nameTr}
+                                        {language === "en" ? feature.nameEn : feature.nameTr}
                                     </div>
                                 </div>
                             ))
@@ -153,7 +151,6 @@ function Header({sendDataToParent}) {
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
---
+++ src/layouts/quick-reservation.jsx
@@ -0,0 +1,51 @@
+import DatePicker from "@/components/ui/DatePicker.jsx";
+import {Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.jsx";
+import {Button} from "@/components/ui/button.jsx";
+import {useTranslation} from "react-i18next";
+import {useLanguage} from "@/components/ui/language-provider.jsx";
+
+function QuickReservation() {
+    const { language } = useLanguage();
+    const {t} = useTranslation();
+
+    return (
+        <div className="quick-reservation flex flex-col gap-5 justify-start items-center w-screen h-[30vh]">
+            <div className="text-3xl font-bold">{t("home.reservation.title")}</div>
+            <div className="container w-[75vw] h-[8vh] flex justify-center shadow-md rounded-md">
+                <DatePicker locale={language} placeholder={t("home.reservation.checkin")} className="h-[8vh] border-0 bg-white border-r rounded-none" />
+                <DatePicker locale={language} placeholder={t("home.reservation.checkout")} className="h-[8vh] border-0 bg-white border-r rounded-none" />
+                <Select>
+                    <SelectTrigger className="w-[15vw] hover:bg-accent hover:text-foreground! h-full border-0 border-r bg-white rounded-none">
+                        <SelectValue placeholder={t("home.reservation.adults")} />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="1">1</SelectItem>
+                            <SelectItem value="2">2</SelectItem>
+                            <SelectItem value="3">3</SelectItem>
+                            <SelectItem value="4">4</SelectItem>
+                            <SelectItem value="5">5</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <Select>
+                    <SelectTrigger className="w-[15vw] hover:bg-accent hover:text-foreground! h-full border-0 border-r bg-white rounded-none">
+                        <SelectValue placeholder={t("home.reservation.kids")} />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="1">1</SelectItem>
+                            <SelectItem value="2">2</SelectItem>
+                            <SelectItem value="3">3</SelectItem>
+                            <SelectItem value="4">4</SelectItem>
+                            <SelectItem value="5">5</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <Button className="w-[15vw] h-full rounded-none">{t("home.reservation.button")}</Button>
+            </div>
+        </div>
+    );
+}
+
+export default QuickReservation;
